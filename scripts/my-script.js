3412e6f6944ebc8b303e07a2aac8336a57248c62
Bratislav Ranđelović <daglasadams@gmai.com>
2018-01-21 04:03:11 +0100

    fixed script so the get-quote button works

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,13 +1,16 @@
-function updateQuote(){
+document.getElementById("get-quote-btn").onclick = function (){
     var path = "https://talaikis.com/api/quotes/random/";
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(this.status == 200 && request.readyState == 4){
-            var quote = JSON.parse(this.quote);
-            var author = JSON.parse(this.author);
+            console.log("Parsing");
+            var response = JSON.parse(this.responseText);
+            var quote = response.quote;
+            console.log(quote);
+            var author = response["author"];
             document.getElementById("quote").innerHTML = quote;
         }
     };
     request.open("GET", path);
-}
-document.getElementById("get-quote-btn").onclick = updateQuote;
+    request.send();
+};
