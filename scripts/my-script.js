fbe640c891a0237e1f8d192826d08fdd05602c8c
Bratislav Ranđelović <daglasadams@gmai.com>
2018-01-21 03:24:16 +0100

    added ajax to update quote

---
+++ scripts/my-script.js
@@ -0,0 +1,11 @@
+function updateQuote(){
+    var path = "https://talaikis.com/api/quotes/random/";
+    var request = new XMLHttpRequest();
+    request.onreadystatechange(function(){
+        if(this.status == 200 && request.readyState == 4){
+            var quote = JSON.parse(this.quote);
+            var author = JSON.parse(this.author);
+            document.getElementById("quote").innerHTML = quote;
+        }
+    });
+}
