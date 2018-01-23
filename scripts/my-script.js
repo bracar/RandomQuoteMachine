af7ff04a90a885d37b88e38157ba9b618b519961
Bratislav Ranđelović <daglasadams@gmai.com>
2018-01-23 23:17:34 +0100

    show local quote if AJAX is not working

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,4 +1,7 @@
-var quote = quotes[1];
+var getRandom = function(){
+    return Math.floor(Math.random() * quotes.length);
+}; 
+var quote = quotes[getRandom()];
 var showQuote = function (quote){
     document.getElementById("quote").innerHTML ='"'+ quote.quote + '"';
     document.getElementById("author").innerText = quote.author;
@@ -6,22 +9,25 @@ var showQuote = function (quote){
 var updateQuote = function (){
     var path = "https://talaikis.com/api/quotes/random/";
     var request = new XMLHttpRequest();
+    // get quote AJAX or local
     request.onreadystatechange = function(){
         if(this.status == 200 && request.readyState == 4){
-            console.log("Parsing");
             var response = JSON.parse(this.responseText);
             showQuote(response);
+        }else{
+            showQuote(quotes[getRandom()]);
         }
     };
     request.open("GET", path);
     request.send();
 };
 
-updateQuote();
+
 
 document.getElementById("get-quote-btn").onclick = updateQuote;
 
 $(document).ready(function(){
+    showQuote(quotes[getRandom()]);
     $("#tweet-btn").on("click touchstart", function () {
         var url = "https://twitter.com/intent/tweet?" + "text=" + encodeURIComponent('"') + encodeURIComponent(quote.text) + encodeURIComponent('"') + encodeURIComponent("\n") + encodeURIComponent(quote.author);
         window.open(url, '_blank');
