88b114b585e8f6c956266381e6f6f4bf3ba89357
Bratislav Ranđelović <daglasadams@gmai.com>
2018-02-04 00:36:40 +0100

    Added basic swipe support

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,21 +1,23 @@
-var getRandom = function(){
+var getRandom = function () {
     return Math.floor(Math.random() * quotes.length);
-}; 
+};
 var quote = quotes[getRandom()];
-var showQuote = function (quote){
-    document.getElementById("quote").innerHTML ='"'+ quote.quote + '"';
+var showQuote = function (quote) {
+    document.getElementById("quote").innerHTML = '"' + quote.quote + '"';
     document.getElementById("author").innerHTML = quote.author;
 };
-
-function updateQuote(){
+function showLocalQuote() {
+    showQuote(quotes[getRandom()]);
+}
+function updateQuote() {
     var path = "https://talaikis.com/api/quotes/random/";
     var request = new XMLHttpRequest();
     // get quote AJAX or local
-    request.onreadystatechange = function(){
-        if(this.status == 200 && request.readyState == 4){
+    request.onreadystatechange = function () {
+        if (this.status == 200 && request.readyState == 4) {
             var response = JSON.parse(this.responseText);
             quote = response;
-        }else{
+        } else {
             quote = quotes[getRandom()];
         }
     };
@@ -24,11 +26,39 @@ function updateQuote(){
     showQuote(quote);
 }
 
-$(document).ready(function(){
-    showQuote(quote);
+$(document).ready(function () {
+    showLocalQuote();
     $("#get-quote-btn").on("click", updateQuote);
     $("#tweet-btn").on("click touchstart", function () {
         var url = "https://twitter.com/intent/tweet?" + "text=" + encodeURIComponent('"') + encodeURIComponent(quote.quote) + encodeURIComponent('"') + encodeURIComponent("\n") + encodeURIComponent(quote.author);
         window.open(url, '_blank');
-     });
+    });
+
+    // handle swipe event 
+    var xDown = null;
+    var yDown = null;
+
+    function handleTouchStart(e) {
+        xDown = e.touches[0].clientX;
+        yDown = e.touches[0].clientY;
+    }
+
+    function handleTouchEnd(e) {
+        var touches = e.changedTouches;
+        var xUp = touches[touches.length-1].clientX;
+        var yUp = touches[touches.length-1].clientY;
+
+        var deltaX = xDown - xUp;
+        var deltaY = yDown - yUp;
+
+        // swipe in any direction 
+        if (Math.abs(deltaX) > 15 || Math.abs(deltaY) > 15) {
+            updateQuote();
+        }
+        xDown = null;
+        yDown = null;
+    }
+
+    $("#quote").on("touchstart", handleTouchStart);
+    $("#quote").on("touchend", handleTouchEnd);
 });
